4c14bcae50161a139d1c1163a6e70182d62840e4
sss <you@example.com>
2026-03-31 19:09:55 +0600

    fix momitoring

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,10 +4,11 @@ from seed.seed import seed_products
 from app.api.v1.router import api_router
 from app.core.config import settings
 from sqlalchemy.exc import ProgrammingError
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Shop API")
 app.include_router(api_router, prefix="/api/v1")
-
+Instrumentator().instrument(app).expose(app, include_in_schema=False, endpoint="/metrics")
 
 @app.on_event("startup")
 async def on_startup():
